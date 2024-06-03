37c7a8e4bf074cd9c53eb105381e01f28ccfbe5c
Harry Dhillon <hdhillon478@gmail.com>
2024-06-03 22:36:18 +0100

    Add malicious <script> validation to text area

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+
 export default function TextArea() {
-  return <textarea className='textarea' />;
+  const [textValue, setTextValue] = useState("");
+
+  function handleChange(event) {
+    let textInput = event.target.value;
+
+    // Check if the input contains the string "<script>"
+    if (textInput.includes("<script>")) {
+      alert("No scripts allowed!");
+      textInput = textInput.replace("<script>", "");
+    }
+    setTextValue(textInput);
+  }
+
+  return (
+    <textarea
+      value={textValue}
+      onChange={handleChange}
+      placeholder='Enter you text...'
+      spellCheck='false'
+      className='textarea'
+    />
+  );
 }
