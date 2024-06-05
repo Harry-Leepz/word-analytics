e0abb6428053731639c95a2862f91cf3846d1d45
Harry Dhillon <hdhillon478@gmail.com>
2024-06-05 18:57:56 +0100

    add common constants file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const FACEBOOK_MAX_CHARS = 2200;
+export const INSTAGRAM_MAX_CHARS = 280;
+
+export const WARNING_MESSAGE = "Scripts are not allowed!";
+
+export const textLabels = {
+  words: "Words",
+  characters: "Characters",
+  instagram: "Instagram",
+  facebook: "Facebook",
+};
