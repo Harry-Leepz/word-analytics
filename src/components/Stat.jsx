f01980e70a9912c91be567f596f5a18d178667df
Harry Dhillon <hdhillon478@gmail.com>
2024-06-05 18:50:12 +0100

    add conditional style change for number limits

--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,7 +1,11 @@
 export default function Stat({ text, number }) {
   return (
     <section className='stat'>
-      <span className='stat__number'>{number}</span>
+      <span
+        className={`stat__number ${number < 0 ? "stat__number--limit" : ""}`}
+      >
+        {number}
+      </span>
       <h2 className='second-heading'>{text}</h2>
     </section>
   );
